ee753abe38108f7eb30fdbb05ce5ee1e79a55d75
Jeremy Dunck <jdunck@gmail.com>
2011-08-28 14:49:08 -0700

    Deploy reload reworked.

--- reload.sh
+++ reload.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-#!/bin/bash
-sudo kill -9 `pgrep node` 2>/dev/null
 cd /home/nko/deploy/
 rm -rf node_modules
 npm install
 
-#sudo nohup node /home/nko/deploy/server.js &
+sudo kill -9 `pgrep node` 2>/dev/null
 
-#sudo /home/nko/node_modules/.bin/forever -l /home/nko/logs/  -o /home/nko/logs/stdout.log -e /home/nko/logs/stderr.log -p /home/nko/run -a -c /usr/local/bin/node /server.js
+#sudo nohup /home/nko/.nvm/v0.4.10/bin/node /home/nko/deploy/server.js &
+
+sudo /home/nko/node_modules/.bin/forever -l /home/nko/logs/  -o /home/nko/logs/stdout.log -e /home/nko/logs/stderr.log -p /home/nko/run -a -c /home/nko/.nvm/v0.4.10/bin/node /home/nko/deploy/server.js &
