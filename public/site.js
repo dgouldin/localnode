4a8c34fd75c5f7c26ec740f0da7881e3b27f421c
Chris Cinelli <Chris.Cinelli@formativelearning.com>
2011-08-28 02:58:11 -0700

    Revamped UI

--- public/site.js
+++ public/site.js
@@ -1,11 +1,26 @@
 
 $("#signup-form").validationEngine();
 var $body = $('body');
-$(window).resize(function(){
-    if ($body.width() > 1200) {
-        $body.css({'background-width':'100%'});
+
+//Make the background stay nice ;-) 
+function AdjustBG(){
+    if ($body.width() > 1600) {
+        $body.css('background-size','100%');
     } else {
-        $body.css({'background-width':''});
+        $body.css('background-size','');
     }
-});
-    
+}
+AdjustBG();
+$(window).resize(AdjustBG);
+
+$("input, textarea").placeholder();
+
+$("#example-config").fancybox();
+$("#test-config").fancybox({
+				'width'				: '75%',
+				'height'			: '75%',
+				'autoScale'			: false
+			});
+$("#proxy-start").fancybox();
+$("#howitworks-id").fancybox();
+
