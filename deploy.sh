84b9b3558a5758aaf2ac2a1aafe12ae41133eb09
Jeremy Dunck <jdunck@gmail.com>
2011-08-28 00:02:01 -0700

    basic deploy script.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+expected_dir="return-of-the-starcr"
+dir=`basename $PWD`
+if [[ $dir != $expected_dir ]] ; then
+	echo cd to the $expected_dir dir.;
+	exit 1;
+fi
+rsync -avz --delete --exclude .git --delete-excluded . \
+	nko@nko:/home/nko/deploy
