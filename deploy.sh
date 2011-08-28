882577f2ec7f3b80c91d215518b423140678e13a
Jeremy Dunck <jdunck@gmail.com>
2011-08-28 00:22:43 -0700

    How-to doc, basic deploy.

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,4 @@ if [[ $dir != $expected_dir ]] ; then
 fi
 rsync -avz --delete --exclude .git --delete-excluded . \
 	nko@nko:/home/nko/deploy
+ssh nko@nko 'kill -9 `pgrep node`; sudo node /home/nko/deploy/server.js'
